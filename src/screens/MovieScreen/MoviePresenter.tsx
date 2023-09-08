04e882036eabf35f857b1522c84085a04d97a237
GyeongJuPark <altnf1524@naver.com>
2023-09-08 14:04:08 +0900

    Section 중간

--- src/screens/MovieScreen/MoviePresenter.tsx
+++ src/screens/MovieScreen/MoviePresenter.tsx
@@ -1,12 +1,45 @@
+import Loader from "../../components/Loader/Loader";
 
 
+interface MoviePresenterProps {
+    nowPlaying: any[] | null;
+    upcoming: any[] | null;
+    popular: any[] | null;
+    topRated: any[] | null;
+    error: string | null;
+    loading : boolean;
+    popularInfinite: any[] | null;
+    nowPlayingInfinite: any[] | null;
+    upcomingInfinite: any[] | null;
+    topRatedInfinite: any[] | null;
+}
 
-const MoviePresenter : React.FC<any>=({
+const MoviePresenter : React.FC<MoviePresenterProps>=({
+    nowPlaying,
+    upcoming,
+    popular,
+    topRated,
 
+    loading,
+
+    nowPlayingInfinite,
+    popularInfinite,
+    upcomingInfinite,
+    topRatedInfinite
+    
 })=>{
-    return (
-        <div>무미 앱임</div>
-    );
+    const { location : {pathname}} = window;
+
+    console.log(nowPlaying);
+
+
+    return loading ? (
+        <Loader/>
+      ) :
+      (
+        <div>무비 앱임</div>
+      )
+
 }
 
 export default MoviePresenter;
