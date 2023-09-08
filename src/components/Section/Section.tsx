94e3114a1868e03ef584cd337b10f2b99d04ff95
GyeongJuPark <altnf1524@naver.com>
2023-09-08 14:32:55 +0900

    feat : create poster

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,64 +1,61 @@
-import React, { Children, ReactNode } from "react";
+import React, { ReactNode } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-
 interface SectionProps {
-    children: ReactNode;
-  }
-
+  children: ReactNode;
+}
 
 const Container = styled.div`
-  padding : 40px;
-  padding-top : 0px;
-  padding-bottom : 0px;
-  margin-top : 15px;
-  animation : sectionAni 0.6s ease-in;
+    padding: 40px;
+    padding-top: 0px;
+    padding-bottom: 0;
+    margin-top: 15px;
+    animation: sectionAni 0.6s ease-in;
 
-  :not(:last-child) {
-    margin-bottom : 20px;
-  }
-
-  @keyframes sectionAni {
-    from {
-        trasnfrom : translateY(25px);
-        opacity : 0;
+    :not(:last-child) {
+        margin-bottom: 20px;
     }
-    to {
-        transform : translateY(0px);
-        opacity : 1;
+
+    @keyframes sectionAni {
+        from {
+        transform: translateY(25px);
+        opacity: 0;
+        }
+        to {
+        transform: translateY(0px);
+        opacity: 1;
+        }
     }
-  }
 
-  @media (max-width : 768px) {
-    padding : 15px;
-    margin-top : 0px;
-  }
-`
-
-const children = styled.div`
-    display : flex;
-    justify-content : center;
-    flex-direction : row;
-    max-width : 2400px;
-    flex-wrap : wrap;
-
-    @media (max-width : 768px) {
-        justify-content : space-between;
+    @media (max-width: 768px) {
+        padding: 15px;
+        margin-top: 0px;
     }
-`
+`;
+
+const Children = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    max-width: 2400px;
+    flex-wrap: wrap;
+
+    @media (max-width: 768px) {
+        justify-content: space-between;
+    }
+`;
 
 const Section: React.FC<SectionProps> = ({ children }) => {
-    return (
-      <Container>
-        <Children>{children}</Children>
-      </Container>
-    );
-  };
+  return (
+    <Container>
+      <Children>{children}</Children>
+    </Container>
+  );
+};
 
 Section.propTypes = {
-    children : PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
-
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
 
 export default Section;
