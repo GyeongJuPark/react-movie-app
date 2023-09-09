6aff6b78b6fe16af2c073c0e8a0f326b64190181
GyeongJuPark <altnf1524@naver.com>
2023-09-09 14:32:56 +0900

    feat : done detail page

--- src/screens/DetailScreen/DetailContainer.tsx
+++ src/screens/DetailScreen/DetailContainer.tsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 import DetailPresenter from "./DetailPresenter";
 import { useParams } from "react-router-dom";
 import withRouter from '../../utils/withRouter';
+import { moviesApi } from "../../api/movie";
 
 
 interface DetailContainerState{
@@ -10,14 +11,14 @@ interface DetailContainerState{
     loading : boolean,
     recommendations : any,
     cast : any,
-    keyword : any,
+    keywords : any,
     backdrops : any,
     posters : any,
     tvDetail2 : any,
     reviews : any,
 }
 
-class DetailContainer extends Component<{}, DetailContainerState>{
+class DetailContainer extends Component<{parmas : number}, DetailContainerState>{
     constructor(props:any) {
         super(props);
         this.state={
@@ -26,7 +27,7 @@ class DetailContainer extends Component<{}, DetailContainerState>{
             loading : true,
             recommendations : [null],
             cast : [],
-            keyword : [],
+            keywords : [],
             reviews : [],
             backdrops : [],
             posters : [],
@@ -34,9 +35,36 @@ class DetailContainer extends Component<{}, DetailContainerState>{
         };
     }
     
+    async componentDidMount() {
+        try{
+            const parsedId = this.props.parmas;
+            
+            const { data : result} = await moviesApi.movieDetail(parsedId);
+            const { data : {results : recommendations} } = await moviesApi.recommendations(parsedId);
+            const { data : {cast}} = await moviesApi.credits(parsedId);
+            const { data : {keywords}} = await moviesApi.keywords(parsedId);
+            const { data : {results : reviews}} = await moviesApi.reviews(parsedId);
+            const { data : {backdrops}, data : {posters} } = await moviesApi.iamges(parsedId);
 
-    render() {
-        return <DetailPresenter />
+            this.setState({
+                result,
+                recommendations,
+                cast,
+                keywords,
+                reviews,
+                backdrops : backdrops && backdrops,
+                posters : posters && posters,
+                loading : false,
+                error : null
+            });
+        }
+        catch(err){
+            this.setState({error : "상세정보를 가져올 수 없습니다."});
+        }
+    }
+
+    render() {      
+        return <DetailPresenter {...this.state} />
     }
 }
 
